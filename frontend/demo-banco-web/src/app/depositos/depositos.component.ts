fac76ca0befbc39aa3e5c2015f2b2b05d1c7a813
jairojorquera <jairojorquera@gmail.com>
2021-09-17 13:33:23 -0300

    Actualiza componente deposito, elimina JSON resp

--- frontend/demo-banco-web/src/app/depositos/depositos.component.ts
+++ frontend/demo-banco-web/src/app/depositos/depositos.component.ts
@@ -20,6 +20,7 @@ export class DepositosComponent implements OnInit {
     let sesion = this.storageService.getSesion();
   }
 
+  
   onDeposito(event?: MouseEvent) {
 
     if (this.monto > 0) {
@@ -31,32 +32,27 @@ export class DepositosComponent implements OnInit {
       transaccion.tipo = 1;
 
       this.transaccionesService.save(transaccion).subscribe(data => {
-        
         Swal.fire({
-          title: 'Deposito exitoso',          
+          title: 'Deposito exitoso',
           icon: 'success',
           timer: 20000,
           showDenyButton: true,
           confirmButtonText: 'Imprimir',
           denyButtonText: 'Cerrar',
-          html:     '<table style="width:100%; text-align: left">'+
-          '<tr><th>ID</th><td>' + data.id +'</td></tr>'+
-          '<tr><th>Fecha</th><td>' + data.fecha.toLocaleDateString() +'</td></tr>'+
-          '<tr><th>Monto</th><td>' + data.monto +'</td></tr>'+
-          '</table>'
-         
-
+          html: '<table style="width:100%; text-align: left">' +
+            '<tr><th>ID</th><td>' + data.id + '</td></tr>' +
+            '<tr><th>Fecha</th><td>' + new Date().toLocaleDateString() + '</td></tr>' +
+            '<tr><th>Monto</th><td>' + data.monto + '</td></tr>' +
+            '</table>'
         }).then((result) => {
           /* Read more about isConfirmed, isDenied below */
           if (result.isConfirmed) {
             window.print()
-          } 
+          }
         })
-//{"id":17,"fecha":"2021-09-17T01:55:02.44","monto":2,"tipo":1,"rut":"16349750-6","rutRelacionado":null}
+
 
       });
-
-
     }
 
 
